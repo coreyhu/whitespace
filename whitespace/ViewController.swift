e27bea63a80f52d051a7753f069a4186186bb321
coreywho <coreyhu@berkeley.edu>
2019-07-19 20:23:34 -0700

    add speech recording

--- whitespace/ViewController.swift
+++ whitespace/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import Speech
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var startButton: UIButton!
+    
+    public let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))!
+    public var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    public var recognitionTask: SFSpeechRecognitionTask?
+    public let audioEngine = AVAudioEngine()
+    @IBOutlet var textView : UITextView!
+    @IBOutlet var recordButton : UIButton!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        recordButton.isEnabled = false
     }
     
     @IBAction func connectToDevice() {
