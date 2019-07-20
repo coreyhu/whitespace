ea52562d5a1f79189cc62990599111ce029bfcee
coreywho <coreyhu@berkeley.edu>
2019-07-20 05:57:49 -0700

    add statistics view controller

---
+++ whitespace/StatisticsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  StatisticsViewController.swift
+//  whitespace
+//
+//  Created by Corey Hu on 7/20/19.
+//  Copyright © 2019 Corey Hu. All rights reserved.
+//
+
+import UIKit
+
+class StatisticsViewController: NSObject {
+
+}
