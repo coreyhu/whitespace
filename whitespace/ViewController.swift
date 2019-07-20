1a0b9f9fa4e40ff414a3f2ffe0a4d2639cf25aab
coreywho <coreyhu@berkeley.edu>
2019-07-19 18:45:16 -0700

    Add start button

--- whitespace/ViewController.swift
+++ whitespace/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func startRecording() {
+        initSpeech();
+        initPosition();
+    }
+}
 
+extension ViewController {
+    
+    func initSpeech() {
+        
+    }
+}
 
+extension ViewController {
+    
+    func initPosition() {
+        
+    }
 }
 
