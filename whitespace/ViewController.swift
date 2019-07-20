8f3b78ba4cf98692ce1e61fcd42d14f51c5d411f
coreywho <coreyhu@berkeley.edu>
2019-07-19 17:48:44 -0700

    Initial Commit

---
+++ whitespace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whitespace
+//
+//  Created by Corey Hu on 7/19/19.
+//  Copyright © 2019 Corey Hu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
